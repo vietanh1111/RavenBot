da1a815302eef9308539c2c71e6e7744a1249954
vietanh1111 <anh.nguyenviet6gl@gmail.com>
2023-04-28 15:08:07 +0700

    [auto-commit] update data

--- app.js
+++ app.js
@@ -867,7 +867,7 @@ async function piggyBank(jsonData, extra_data = "") {
     let self_report = true;
     for (var member of Object.keys(team_member)) {
         if (jsonData.text.includes(team_member[member]["name"]) || jsonData.text.includes(team_member[member]["alias"])) {
-            console.log(piggyDataJson[currentDate])
+            console.log(piggyDataJson[currentDate][team_member[member]["name"]])
             if (!piggyDataJson[currentDate][team_member[member]["name"]]) {
                 console.log("aaaaa1")
                 myData[currentDate][team_member[member]["name"]] = 3

382efc46328ebba091efd28425f56cb749e0d6a3
vietanh1111 <anh.nguyenviet6gl@gmail.com>
2023-04-28 16:31:00 +0700

    [auto-commit] update data

--- app.js
+++ app.js
@@ -943,13 +943,13 @@ async function getPiggyBank(current_user, mode = "") {
     console.log("getPiggyBank")
     console.log(result)
     let i = 1
-    let biggest = 0
+    let smaller = 1000000
     for (let key in result) {
-        if(biggest < result[key]){
-            i = i + 1
-            biggest = result[key]
-        }  
         msg = msg + "\n| " + key + " | " + result[key] + " | " + i + " |"
+        if(smaller < result[key]){
+            i = i + 1
+            smaller = result[key]
+        }  
     }
     // printLog(arguments.callee.name, JSON.stringify(result, null, 3))
 
